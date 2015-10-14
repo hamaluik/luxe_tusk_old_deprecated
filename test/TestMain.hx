40510c83038e1d9a4e58363f804463beeb72e66e
Kenton Hamaluik <hamaluik@ualberta.ca>
2015-10-14 13:47:55 -0600

    Fixed test runner to reflect that we don't have any tests yet

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -8,15 +8,9 @@ class TestMain implements Buddy<[Tests]>{}
 
 class Tests extends BuddySuite {
 	public function new() {
-		describe('Using Tusk', {
-			var tusk:tusk.Tusk = new Tusk();
-
-			it('should correctly add positive integers', {
-				tusk.add(1, 2).should.be(3);
-			});
-			it('should correctly add positive decimals', {
-				tusk.add(1.3, 4.1).should.beCloseTo(5.4, 2);
-			});
+		describe('Using tusk', {
+			it('should allow rapid development');
+			it('should generate better test cases');
 		});
 	}
 }
