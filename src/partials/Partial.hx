fa78c8ad067de5fad222a40bfa6a99996013b944
Kenton Hamaluik <hamaluik@ualberta.ca>
2015-10-29 16:21:42 -0600

    Refactored partials into its own library

--- src/partials/Partial.hx
+++
@@ -1,9 +0,0 @@
-package partials;
-
-/**
- * Implement this interface to indicate that part of the class is defined somewhere else.
- */
-@:autoBuild(partials.Partials.process())
-interface Partial {
-	
-}
